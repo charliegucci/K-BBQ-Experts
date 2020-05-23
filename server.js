7cbac618b642ee346771673e6e1e3ad68ae37ce5
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-05-23 19:42:20 +1000

    Create GitIgnore#Install node packages#Setup mongoDB Atlas#

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+//connect Database
+connectDB();
+
+// Init Middleware
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+
+// Define Port Number
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
