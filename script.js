f84779c0041078140f19e8c480e66036d57e30bb
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-05-25 18:15:27 +1000

    Added Articles Model and Change Settings back to Scrapper

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const root_url = 'http://localhost:5000';
 
-let country = 'au'
-
-fetch(root_url+`/news/${country}`)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-        // Wilson- you can start manipulating the DOM here
-        // Data is an array of news article objects
-
-    })
-    .catch((err) => console.log(err))
+fetch(root_url + `/scrape`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    // Wilson- you can start manipulating the DOM here
+    // Data is an array of news article objects
+  })
+  .catch((err) => console.log(err));
