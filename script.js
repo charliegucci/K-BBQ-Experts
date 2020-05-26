53ada4b5b302fe9f41e0faf5933c00f6fc57e504
Wilson Esmundo <wilson.esmundo@aussiebroadband.com.au>
2020-05-26 10:29:46 +1000

    DOm Manipulation

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ fetch(root_url + `/scrape`)
     console.log(data);
     // Wilson- you can start manipulating the DOM here
     // Data is an array of news article objects
+    let list = document.getElementById('news');
+    for (i = 0; i < data.length; i++) {
+      let headlines = document.createElement('h2');
+      headlines.innerHTML = data[i].heading;
+      list.appendChild(headlines);
+      let content = document.createElement('p');
+      content.innerHTML = data[i].content;
+      list.appendChild(content);
+      let reference = document.createElement('a');
+      reference.setAttribute('href', data[i].url);
+      reference.setAttribute('target', '_blank');
+      reference.innerHTML = data[i].url;
+      list.appendChild(reference);
+    }
   })
   .catch((err) => console.log(err));
 
