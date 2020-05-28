c25f4a230011e504458c491af85f8199cc622a8e
JW888 <jeb.williams8@gmail.com>
2020-05-28 10:34:54 +1000

    update to scraper to add images to ABC news site articles

--- scrape.js
+++ scrape.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
-const re = new RegExp('(covid\-19|coronavirus|corona|covid)', 'i')
+const reCovid = new RegExp('(covid\-19|coronavirus|corona|covid)', 'i')
+const reImg = new RegExp('(.jpg|.jpeg|.png|)')
+const numAritcle = 20
 
 
 async function getNews () {
@@ -9,106 +11,107 @@ async function getNews () {
     //async function for abc.com.au
     async function headlinesABC () {
 
-    const browser1 = await puppeteer.launch({headless:true})
-    const page = await browser1.newPage()
+        const browser = await puppeteer.launch({headless:false})
+        const page = await browser.newPage()
+        const navigationPromise = page.waitForNavigation()
+        
+        await page.goto('https://www.abc.net.au/news/justin/')
 
-    await page.setRequestInterception(true);
-    page.on('request', request => {
-        if (request.resourceType() === 'script')
-        request.abort();
-        else
-        request.continue();
-    });
+        //removes pagination from the ABC news site
+        for (i = 0; i < 10; i++) {
+            await page.waitForSelector('.GvLLo > .\_27tpe > .\_6TJ0W > .\_3XQNk > .\_1cg3S')
+            await page.click('.GvLLo > .\_27tpe > .\_6TJ0W > .\_3XQNk > .\_1cg3S')
+        }
+        
+        for (i = 1; i < numAritcle; i++ ){
 
-    const navigationPromise = page.waitForNavigation()
-    await page.goto('https://www.abc.net.au/news/justin/')
-    //   await page.setViewport({ width: 1200, height: 800 })
+            await navigationPromise
+            await page.waitForSelector(`.\_2kqX-:nth-child(${i}) > .\_1E-Sb > .\_38hRP > div > .\_2HoMm > .zT1nT > .MLQqg`)
+            await page.click(`.\_2kqX-:nth-child(${i}) > .\_1E-Sb > .\_38hRP > div > .\_2HoMm > .zT1nT > .MLQqg`)
+            await page.waitForSelector('title')
+            await page.waitForSelector('p')
+            await page.waitForSelector('img')
 
-    let title
+            const textsJoined = await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText).join('\n'));
 
-    for (i = 0; i < 10; i++) {
-        await page.waitForSelector('.GvLLo > .\_27tpe > .\_6TJ0W > .\_3XQNk > .\_1cg3S')
-        await page.click('.GvLLo > .\_27tpe > .\_6TJ0W > .\_3XQNk > .\_1cg3S')
-    }
-    
-    for (i = 1; i < 8; i++ ){
+            if (await reCovid.test(textsJoined) === false) {
 
-        await navigationPromise
-
-        await page.waitForSelector(`.\_2kqX-:nth-child(${i}) > .\_1E-Sb > .\_38hRP > div > .\_2HoMm > .zT1nT > .MLQqg`)
-        await page.click(`.\_2kqX-:nth-child(${i}) > .\_1E-Sb > .\_38hRP > div > .\_2HoMm > .zT1nT > .MLQqg`)
-        await page.waitForSelector('title')
-        await page.waitForSelector('p')
-        title = await page.$eval('title', title => title.innerHTML);
-        const textsJoined = await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText).join('\n'));
-        if (await re.test(textsJoined) === false) {
-            articles.push({'heading':title,
-                        'content': await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText)),
-                        'url': page.url()
+                //gets all of the first articles on the page, however it only returns the first image that is either a .jpg, .jpeg, or .png
+                let img
+                try {
+                    const imgs = await page.evaluate(() => Array.from(document.images, e => e.src))
+                    for (ig of imgs) {
+                        if (await reImg.test(ig) === true) {
+                            img = ig
+                            break
+                        }
+                    }
+                } catch {
+                    console.log("No Images")
+                    img = 'No Images'
+                }
+                
+                articles.push({'heading': await page.$eval('title', title => title.innerHTML),
+                            'content': await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText)),
+                            'url': page.url(),
+                            'img': img
                         })
-        } 
+            } 
 
-        await navigationPromise
-        await page.goBack()
+            await navigationPromise
+            await page.goBack()
 
-    }
-    
-    await browser1.close()
-    console.log(articles.length)
+        }
+        
+        await browser.close()
+        console.log(articles.length)
 
     }
 
     //async function for news.com.au
     async function headlinesNEWS () {
 
-    const browser2 = await puppeteer.launch({headless:true})
-    const page = await browser2.newPage()
+        const browser = await puppeteer.launch({headless:false})
+        const page = await browser.newPage()
 
-    await page.setRequestInterception(true);
-    page.on('request', request => {
-        if (request.resourceType() === 'script')
-        request.abort();
-        else
-        request.continue();
-    });
+        // remove javascript functionality to improve the speed
+        await page.setRequestInterception(true);
+        page.on('request', request => {
+            if (request.resourceType() === 'script')
+            request.abort();
+            else
+            request.continue();
+        });
 
-
-    const navigationPromise = page.waitForNavigation()
-    await page.goto('https://www.news.com.au/national/breaking-news')
-    //   await page.setViewport({ width: 1200, height: 800 })
-
-    let title
-    
-    for (i = 100; i > 92; i-- ){
-
-        await navigationPromise
-
-        await page.waitForSelector(`.module-content > .cirpos-${i} > .story-block > .heading > a`)
-        await page.click(`.module-content > .cirpos-${i} > .story-block > .heading > a`)
+        const navigationPromise = page.waitForNavigation()
+        await page.goto('https://www.news.com.au/national/breaking-news')
         
-        await page.waitForSelector('title')
-        await page.waitForSelector('p')
-        title = await page.$eval('title', title => title.innerHTML);
-        const textsJoined = await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText).join('\n'));
-        if (await re.test(textsJoined) === false) {
-            articles.push({'heading':title,
-                        'content': await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText)),
-                        'url': page.url()
-                        })
-        } 
+        for (i = 100; i > (100 - numAritcle); i-- ){
 
-        await navigationPromise
-        await page.goBack()
+            await navigationPromise
+            await page.waitForSelector(`.module-content > .cirpos-${i} > .story-block > .heading > a`)
+            await page.click(`.module-content > .cirpos-${i} > .story-block > .heading > a`)
+            await page.waitForSelector('title')
+            await page.waitForSelector('p')
+            let title = await page.$eval('title', title => title.innerHTML);
+            const textsJoined = await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText).join('\n'));
+            if (await reCovid.test(textsJoined) === false) {
+                articles.push({'heading':title,
+                            'content': await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText)),
+                            'url': page.url()
+                            })
+            } 
+
+            await navigationPromise
+            await page.goBack()
+
+        }
+        
+        await browser.close()
+        console.log(articles.length)
 
     }
-    
-    await browser2.close()
 
-    console.log(articles.length)
-
-    }
-
-    
    await headlinesABC()
    await headlinesNEWS()
 
@@ -117,6 +120,132 @@ async function getNews () {
 }
 
 module.exports = { getNews }
+
+
+
+///////////////////////////////////
+////Previous Verison///////////////
+///////////////////////////////////
+
+// const puppeteer = require('puppeteer');
+// const re = new RegExp('(covid\-19|coronavirus|corona|covid)', 'i')
+
+
+// async function getNews () {
+
+//     const articles = []
+
+//     //async function for abc.com.au
+//     async function headlinesABC () {
+
+//     const browser1 = await puppeteer.launch({headless:true})
+//     const page = await browser1.newPage()
+
+//     await page.setRequestInterception(true);
+//     page.on('request', request => {
+//         if (request.resourceType() === 'script')
+//         request.abort();
+//         else
+//         request.continue();
+//     });
+
+//     const navigationPromise = page.waitForNavigation()
+//     await page.goto('https://www.abc.net.au/news/justin/')
+//     //   await page.setViewport({ width: 1200, height: 800 })
+
+//     let title
+
+//     for (i = 0; i < 10; i++) {
+//         await page.waitForSelector('.GvLLo > .\_27tpe > .\_6TJ0W > .\_3XQNk > .\_1cg3S')
+//         await page.click('.GvLLo > .\_27tpe > .\_6TJ0W > .\_3XQNk > .\_1cg3S')
+//     }
+    
+//     for (i = 1; i < 8; i++ ){
+
+//         await navigationPromise
+
+//         await page.waitForSelector(`.\_2kqX-:nth-child(${i}) > .\_1E-Sb > .\_38hRP > div > .\_2HoMm > .zT1nT > .MLQqg`)
+//         await page.click(`.\_2kqX-:nth-child(${i}) > .\_1E-Sb > .\_38hRP > div > .\_2HoMm > .zT1nT > .MLQqg`)
+//         await page.waitForSelector('title')
+//         await page.waitForSelector('p')
+//         title = await page.$eval('title', title => title.innerHTML);
+//         const textsJoined = await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText).join('\n'));
+//         if (await re.test(textsJoined) === false) {
+//             articles.push({'heading':title,
+//                         'content': await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText)),
+//                         'url': page.url()
+//                         })
+//         } 
+
+//         await navigationPromise
+//         await page.goBack()
+
+//     }
+    
+//     await browser1.close()
+//     console.log(articles.length)
+
+//     }
+
+//     //async function for news.com.au
+//     async function headlinesNEWS () {
+
+//     const browser2 = await puppeteer.launch({headless:true})
+//     const page = await browser2.newPage()
+
+//     await page.setRequestInterception(true);
+//     page.on('request', request => {
+//         if (request.resourceType() === 'script')
+//         request.abort();
+//         else
+//         request.continue();
+//     });
+
+
+//     const navigationPromise = page.waitForNavigation()
+//     await page.goto('https://www.news.com.au/national/breaking-news')
+//     //   await page.setViewport({ width: 1200, height: 800 })
+
+//     let title
+    
+//     for (i = 100; i > 92; i-- ){
+
+//         await navigationPromise
+
+//         await page.waitForSelector(`.module-content > .cirpos-${i} > .story-block > .heading > a`)
+//         await page.click(`.module-content > .cirpos-${i} > .story-block > .heading > a`)
+        
+//         await page.waitForSelector('title')
+//         await page.waitForSelector('p')
+//         title = await page.$eval('title', title => title.innerHTML);
+//         const textsJoined = await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText).join('\n'));
+//         if (await re.test(textsJoined) === false) {
+//             articles.push({'heading':title,
+//                         'content': await page.evaluate(() => [...document.querySelectorAll('p')].map(elem => elem.innerText)),
+//                         'url': page.url()
+//                         })
+//         } 
+
+//         await navigationPromise
+//         await page.goBack()
+
+//     }
+    
+//     await browser2.close()
+
+//     console.log(articles.length)
+
+//     }
+
+    
+//    await headlinesABC()
+//    await headlinesNEWS()
+
+//    return articles
+
+// }
+
+// module.exports = { getNews }
 
 
 // getNews().then((data) =>{
